10734337a859e648b20133b0a192ff088f9b26cf
Vijayasekharr <vijayasekhargit@gmail.com>
2022-11-28 14:40:15 +0530

    [ First - Commit ]

---
+++ Admin-Server-Application/src/test/java/com/sbms/AdminServerApplication/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbms.AdminServerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
