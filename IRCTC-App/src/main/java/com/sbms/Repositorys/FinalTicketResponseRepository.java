63ca03db846e1c3a5f39dbae38e793c265c053a4
Vijayasekharr <vijayasekhargit@gmail.com>
2022-12-13 00:04:41 +0530

    Changes in Search Functionality

--- IRCTC-App/src/main/java/com/sbms/Repositorys/FinalTicketResponseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sbms.Repositorys;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sbms.Entitys.FinalTicketResponse;
-
-public interface FinalTicketResponseRepository extends JpaRepository<FinalTicketResponse, Integer> {
-
-}
